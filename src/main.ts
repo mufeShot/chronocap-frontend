06da4c1fe0812025757b7e278db2ca840e609a07
Aljaz <aljazfruhvirt@gmail.com>
2025-08-23 03:03:21 +0200

    Update 2.0

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { useAuth } from './composables/useAuth'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+
+const { initAuth } = useAuth()
+initAuth()
+
+app.mount('#app')
